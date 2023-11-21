33d3c935deecd3a9cbb04878d661fd1c725234f2
kubolion <144912772+kubolion@users.noreply.github.com>
2023-11-21 16:26:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<form action="" method="">
+<input type="text" name="login"
+/>
+<input type="password" name="pass"
+/>
+<input type="email" name="email" />
+<input type="tel" name="tel" />
+<input type="submit" value="Отправить данные"/>
+</form>
